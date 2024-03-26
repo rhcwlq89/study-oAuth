824d29c0acdffea6ec77dc915c66a1208c42a5cf
hojongLee <hojong.lee@fastfive.co.kr>
2024-03-26 14:49:24 +0900

    git init

---
+++ src/main/kotlin/rhcwlq89/study/auth/AuthApplication.kt
@@ -0,0 +1,11 @@
+package rhcwlq89.study.auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthApplication>(*args)
+}
